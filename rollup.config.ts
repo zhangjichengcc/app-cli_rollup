90b64bb12c168861528d770b3e9d84a8aad0301f
zhangjicheng <zhangjichengcc@163.com>
2023-02-07 00:45:34 +0800

    update config

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,6 +3,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import alias from "@rollup/plugin-alias";
+import babel from "@rollup/plugin-babel";
 import { createRequire } from "node:module";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
@@ -13,11 +14,28 @@ const __dirname = path.dirname(__filename);
 const pkg = require("./package.json");
 
 const plugins: RollupOptions["plugins"] = [
-  nodeResolve(),
-  commonjs(),
-  typescript(),
   alias({
     entries: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
+  }),
+  nodeResolve(),
+  commonjs(),
+  typescript({
+    declarationDir: "./declare",
+  }),
+  // ! babel 不生效，待处理
+  babel({
+    babelHelpers: "bundled",
+    exclude: ["node_modules/**"],
+    presets: [
+      [
+        "@babel/env",
+        "@babel/preset-typescript",
+        {
+          // "modules": false ，否则 Babel 会在 Rollup 有机会做处理之前，将我们的模块转成 CommonJS ，导致 Rollup 的一些处理失败。
+          modules: false,
+        },
+      ],
+    ],
   }),
 ];
 
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
 export { now, formatDate } from "@/utils/index";
+const hello = "Hi, enjoy your self";
+
+console.log(hello);
